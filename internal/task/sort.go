857eba19b1ddfd2bc2112b2e59c5530443c14b0b
Darwin Froese <froese.darwin@gmail.com>
2025-06-01 14:39:43 -0500

    sorts by description if dates are the same to keep sorting constant

--- internal/task/sort.go
+++ internal/task/sort.go
@@ -1,6 +1,9 @@
 package task
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 const (
 	sortOrderDesc = iota
@@ -9,10 +12,17 @@ const (
 
 func (service *Service) sortOrderCompletedDateFunc(sortOrder int) func(a, b int) int {
 	return func(a, b int) int {
-		taskA := service.getTask(a).CompletedAt.Round(24 * time.Hour)
-		taskB := service.getTask(b).CompletedAt.Round(24 * time.Hour)
+		taskA := service.getTask(a)
+		taskB := service.getTask(b)
 
-		order := taskA.Compare(taskB)
+		taskADate := taskA.CompletedAt.Round(24 * time.Hour)
+		taskBDate := taskB.CompletedAt.Round(24 * time.Hour)
+
+		order := taskADate.Compare(taskBDate)
+
+		if order == 0 {
+			order = strings.Compare(taskA.Description, taskB.Description)
+		}
 
 		if sortOrder == sortOrderDesc {
 			return order * -1
